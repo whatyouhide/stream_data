bd4764d9ea2f0fe2e1375f148730ad782d3566c3
Andrea Leopardi <an.leopardi@gmail.com>
2017-06-09 22:33:42 +0200

    Clean up Stream.Data.Random

--- lib/stream/data/random.ex
+++ lib/stream/data/random.ex
@@ -1,12 +1,12 @@
 defmodule Stream.Data.Random do
+  @algorithm :exs1024
+
   def split(seed) do
-    {int_tuple1, seed} = triple_of_ints(seed)
-    {int_tuple2, _seed} = triple_of_ints(seed)
-
-    seed1 = :rand.seed_s(:exs64, int_tuple1)
-    seed2 = :rand.seed_s(:exs64, int_tuple2)
-
-    {seed1, seed2}
+    {int1, seed} = :rand.uniform_s(1_000_000_000, seed)
+    {int2, seed} = :rand.uniform_s(1_000_000_000, seed)
+    {int3, seed} = :rand.uniform_s(1_000_000_000, seed)
+    new_seed = :rand.seed_s(@algorithm, {int1, int2, int3})
+    {new_seed, seed}
   end
 
   def uniform_in_range(left..right, seed) when left > right do
@@ -17,12 +17,5 @@ defmodule Stream.Data.Random do
     width = right - left
     {random_int, _seed} = :rand.uniform_s(width + 1, seed)
     random_int - 1 + left
-  end
-
-  defp triple_of_ints(seed) do
-    {int1, seed} = :rand.uniform_s(1_000_000_000, seed)
-    {int2, seed} = :rand.uniform_s(1_000_000_000, seed)
-    {int3, seed} = :rand.uniform_s(1_000_000_000, seed)
-    {{int1, int2, int3}, seed}
   end
 end
