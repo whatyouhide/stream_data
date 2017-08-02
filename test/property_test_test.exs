a160baa512b5294baeadb2783ffd43ab8f187caf
Andrea Leopardi <an.leopardi@gmail.com>
2017-08-02 15:53:40 +0200

    Add some tests for PropertyTest macros

---
+++ test/property_test_test.exs
@@ -0,0 +1,32 @@
+defmodule PropertyTestTest do
+  use ExUnit.Case, async: true
+
+  import PropertyTest
+
+  property "shrinking" do
+    assert_raise ExUnit.AssertionError, fn ->
+      check all list <- list_of(int()) do
+        assert 5 not in list
+      end
+    end
+  end
+
+  property "works with errors that are not assertion errors" do
+    assert_raise PropertyTest.Error, fn ->
+      check all tuple <- {:ok, nil} do
+        :ok = tuple
+      end
+    end
+  end
+
+  property "runs the number of specified times" do
+    {:ok, counter} = Agent.start_link(fn -> 0 end)
+
+    check all :ok <- :ok, max_runs: 10 do
+      Agent.update(counter, &(&1 + 1))
+      :ok
+    end
+
+    assert Agent.get(counter, &(&1)) == 10
+  end
+end
