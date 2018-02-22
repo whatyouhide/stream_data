765cf0e3e733f78969ec14202fe8fb78d6ee296b
Andrea Leopardi <an.leopardi@gmail.com>
2018-02-22 11:53:50 -0800

    Create a test/stdlib directory and split stdlib tests by module

--- test/stdlib/string_test.exs
+++ test/stdlib/string_test.exs
@@ -1,4 +1,4 @@
-defmodule StdlibPropertiesTest do
+defmodule StreamData.StringTest do
   use ExUnit.Case, async: true
   use ExUnitProperties
 
@@ -14,14 +14,15 @@ defmodule StdlibPropertiesTest do
     end
   end
 
-  # This is expected to fail until https://github.com/elixir-lang/elixir/issues/7023 gets fixed.
-  @tag :skip
-  property "String.replace/3 is equivalent to String.split/1 + Enum.join/2" do
-    check all string <- string(:printable),
-              pattern <- string(:printable),
-              replacement <- string(:printable) do
-      assert String.replace(string, pattern, replacement) ==
-               string |> String.split(pattern) |> Enum.join(replacement)
+  if Version.match?(System.version(), "~> 1.6") do
+    # From https://github.com/elixir-lang/elixir/issues/7023.
+    property "String.replace/3 is equivalent to String.split/1 + Enum.join/2" do
+      check all string <- string(:printable),
+                pattern <- string(:printable),
+                replacement <- string(:printable) do
+        assert String.replace(string, pattern, replacement) ==
+                 string |> String.split(pattern) |> Enum.join(replacement)
+      end
     end
   end
 end
