566b88967ece409520b7c8a84ac54b31ba526d6c
Qqwy / Marten <w-m@wmcode.nl>
2022-05-30 12:46:37 +0200

    Update test/stream_data_test.exs
    Co-authored-by: Andrea Leopardi <hi@andrealeopardi.com>

--- test/stream_data_test.exs
+++ test/stream_data_test.exs
@@ -446,7 +446,7 @@ defmodule StreamDataTest do
   property "nonempty_improper_list_of/2" do
     check all list <- nonempty_improper_list_of(integer(), constant("")) do
       assert list != []
-      assert list != [""]
+      refute match?([_], list)
       each_improper_list(list, &assert(is_integer(&1)), &assert(&1 == ""))
     end
   end
