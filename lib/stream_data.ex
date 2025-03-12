167d9afee0e1b310c355188b5adf5665a36aa96b
spicychickensauce <spicychickensauce@nope.com>
2025-03-12 11:30:39 +0100

    Use clojure implementation of shuffle to get desired shrinking

--- lib/stream_data.ex
+++ lib/stream_data.ex
@@ -1155,14 +1155,29 @@ defmodule StreamData do
 
   Shrinks towards not changed lists.
   """
+  def shuffle([]), do: constant([])
+
   def shuffle(list) do
-    list_of(float(), length: length(list))
-    |> map(fn sort ->
-      Enum.zip(list, sort)
-      |> Enum.sort_by(&elem(&1, 1))
-      |> Enum.unzip()
-      |> elem(0)
+    # convert to array for faster swapping
+    array = :array.from_list(list)
+    l = :array.size(array)
+
+    # Inspired by this clojure implementation:
+    # https://github.com/clojure/test.check/blob/0ee576eb73d4864c199305c4a0c1e8101d8d1b39/src/main/clojure/clojure/test/check/generators.cljc#L636
+    list_of({integer(0..(l - 1)), integer(0..(l - 1))}, length: 0..(2 * l))
+    |> map(fn swap_instructions ->
+      Enum.reduce(swap_instructions, array, fn {i, j}, array ->
+        array_swap(array, i, j)
+      end)
+      |> :array.to_list()
     end)
+  end
+
+  defp array_swap(array, i, j) do
+    v_i = :array.get(i, array)
+    v_j = :array.get(j, array)
+    array = :array.set(i, v_j, array)
+    :array.set(j, v_i, array)
   end
 
   @doc ~S"""
