2a6863c2ac142da208529c00778be5edf47ed88c
Andrea Leopardi <an.leopardi@gmail.com>
2026-07-14 11:51:47 +0200

    Require Elixir 1.14+

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule StreamData.Mixfile do
     [
       app: :stream_data,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
